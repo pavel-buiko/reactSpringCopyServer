d245eb09a3feb9e6b44cafc9c02288453ef18691
Pavel Buiko <pashabuiko17102004@gmail.com>
2024-10-03 14:11:22 +0300

    Add new vision of image import

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-app.use("/img", express.static("img"));
+app.use("/img", express.static("./img"));
 app.use(express.json());
 
 app.get("/api/test", (_, res) => {
