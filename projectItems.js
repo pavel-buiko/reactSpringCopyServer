6cf5396b23188599a66bbad5ba127ad00510f8e5
Pavel Buiko <pashabuiko17102004@gmail.com>
2024-10-03 14:15:34 +0300

    Fix image path to relative static resources

--- projectItems.js
+++ projectItems.js
@@ -3,44 +3,42 @@ const projectItems = [
     title: "Spring Boot",
     description:
       "Takes an opinionated view of building Spring applications and gets you in and running as quickly as possible.",
-    imgSrc: "/img/spring-boot.svg",
+    imgSrc: "ClientSpring/img/spring-boot.svg",
     link: "#",
   },
   {
     title: "Spring Framework",
     description:
       "Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more.",
-    imgSrc:
-      "https://server-ancient-grass-9030.fly.dev/img/spring-framework.svg",
+    imgSrc: "ClientSpring/img/spring-framework.svg",
     link: "#",
   },
   {
     title: "Spring Data",
     description:
       "Provides a consistent approach to data access - relational, non-relational, map-reduce, and beyond.",
-    imgSrc: "https://server-ancient-grass-9030.fly.dev/img/spring-data.svg",
+    imgSrc: "ClientSpring/img/spring-data.svg",
     link: "#",
   },
   {
     title: "Spring Cloud",
     description:
       "Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.",
-    imgSrc: "https://server-ancient-grass-9030.fly.dev/img/spring-cloud.svg",
+    imgSrc: "ClientSpring/img/spring-cloud.svg",
     link: "#",
   },
   {
     title: "Spring Cloud Data Flow",
     description:
       "Provides an orchestration service for composable data microservice applications on modern runtimes.",
-    imgSrc:
-      "https://server-ancient-grass-9030.fly.dev/img/spring-data-flow.svg",
+    imgSrc: "ClientSpring/img/spring-data-flow.svg",
     link: "#",
   },
   {
     title: "Spring Security",
     description:
       "Takes an opinionated view of building Spring applications and gets you in and running as quickly as possible.",
-    imgSrc: "https://server-ancient-grass-9030.fly.dev/img/spring-security.svg",
+    imgSrc: "ClientSpring/img/spring-security.svg",
     link: "#",
   },
 ];
